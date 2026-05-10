e09790ff3764af67e7f1c1e525edb0b1eae605c2
jkosimov806-creator <jkosimov806@gmail.com>
2026-05-10 23:27:57 +0500

    Update main.py

--- main.py
+++ main.py
@@ -8,25 +8,28 @@ from handlers.start import start_handler
 from handlers.menu import main_menu_handler
 from handlers.catalog import get_catalog_handlers
 
-TOKEN = "8709726103:AAEqLzikyRxkusEyulxhvmL5sJ1Ivcb_THs"
+TOKEN = "8709726103:AAE_REPLACE_WITH_REAL_TOKEN"
 
 
 def main():
     app = Application.builder().token(TOKEN).build()
 
-    # =====================
-    # COMMANDS
-    # =====================
+    # START
     app.add_handler(CommandHandler("start", start_handler))
 
-    # =====================
     # MENU
-    # =====================
     app.add_handler(
         CallbackQueryHandler(main_menu_handler, pattern="^main_menu$")
     )
 
-    # =====================
-    # CATALOG (ВАЖНО)
-    # =====================
-    handlers =
+    # CATALOG
+    for handler in get_catalog_handlers():
+        app.add_handler(handler)
+
+    print("Bot started")
+
+    app.run_polling(drop_pending_updates=True)
+
+
+if __name__ == "__main__":
+    main()
