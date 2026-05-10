8a1a23bd13557902e43683af81f7ed357216cf24
jkosimov806-creator <jkosimov806@gmail.com>
2026-05-10 23:30:18 +0500

    Update main.py

--- main.py
+++ main.py
@@ -8,21 +8,18 @@ from handlers.start import start_handler
 from handlers.menu import main_menu_handler
 from handlers.catalog import get_catalog_handlers
 
-TOKEN = "8709726103:AAE_REPLACE_WITH_REAL_TOKEN"
+TOKEN = "8709726103:AAEqLzikyRxkusEyulxhvmL5sJ1Ivcb_THs"
 
 
 def main():
     app = Application.builder().token(TOKEN).build()
 
-    # START
     app.add_handler(CommandHandler("start", start_handler))
 
-    # MENU
     app.add_handler(
         CallbackQueryHandler(main_menu_handler, pattern="^main_menu$")
     )
 
-    # CATALOG
     for handler in get_catalog_handlers():
         app.add_handler(handler)
 
