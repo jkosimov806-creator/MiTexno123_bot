ee7fab9247cd5174dc3aefce178b88fd104c2a33
jkosimov806-creator <jkosimov806@gmail.com>
2026-05-10 23:57:02 +0500

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,22 @@
+SyntaxError: unterminated triple-quoted string literal (detected at line 35)
+
+10/05/26 23:35:37
+                     ^
+
+10/05/26 23:35:37
+    await db.execute("""
+
+10/05/26 23:35:37
+  File "/app/database.py", line 33
+
+10/05/26 23:35:37
+    import database
+
+10/05/26 23:35:37
+  File "/app/handlers/catalog.py", line 3, in <module>
+
+10/05/26 23:35:37
+    from handlers.catalog import get_catalog_handlers
+
+10/05/26 23:35:37
+  File "/app/main.py", line 9, in <module>
