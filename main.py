dd579804028f9382f386e29008267ad138d3120d
jkosimov806-creator <jkosimov806@gmail.com>
2026-05-11 12:46:33 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
 import asyncio
 from aiogram import Bot, Dispatcher, types, F
-from config import TOKEN
-from database import init_db, db_query
+from config import TOKEN, ADMIN_ID
+import support_h # Импортируем наш новый файл
 from kb import main_kb
-import support_h # Импортируем файл поддержки
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
+# Подключаем роутер поддержки
+dp.include_router(support_h.router)
+
 @dp.message(F.text == "/start")
 async def cmd_start(message: types.Message):
-    db_query('INSERT OR IGNORE INTO users VALUES (?)', (message.from_user.id,))
-    await message.answer("◈ **MI TEXNO** 📱\nВыберите раздел:", reply_markup=main_kb(message.from_user.id))
+    await message.answer("◈ <b>MI TEXNO</b> 📱\nВыберите раздел:", 
+                         reply_markup=main_kb(message.from_user.id, ADMIN_ID), 
+                         parse_mode="HTML")
 
 @dp.callback_query(F.data == "to_main")
-async def back_to_main(c: types.CallbackQuery):
+async def back_home(c: types.CallbackQuery):
     await c.message.delete()
     await cmd_start(c.message)
 
 async def main():
-    init_db()
-    dp.include_router(support_h.router) # Подключаем поддержку
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
