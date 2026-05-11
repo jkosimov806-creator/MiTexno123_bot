ca54bd4166cfb47008b69f21ffa8e443cb37e272
jkosimov806-creator <jkosimov806@gmail.com>
2026-05-11 12:45:13 +0500

    Update kb.py

--- kb.py
+++ kb.py
@@ -1,12 +1,11 @@
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 from aiogram import types
-from config import ADMIN_ID, SUPPORT_LINK
 
-def main_kb(user_id):
+def main_kb(user_id, admin_id):
     kb = InlineKeyboardBuilder()
     kb.row(types.InlineKeyboardButton(text="▻ КАТАЛОГ", callback_data="catalog_0"))
     kb.row(types.InlineKeyboardButton(text="🛒 КОРЗИНА", callback_data="view_cart"))
     kb.row(types.InlineKeyboardButton(text="🛡 ПОДДЕРЖКА", callback_data="support_info"))
-    if user_id == ADMIN_ID:
+    if user_id == admin_id:
         kb.row(types.InlineKeyboardButton(text="⚙️ АДМИНКА", callback_data="admin_menu"))
     return kb.as_markup()
